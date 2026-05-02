2691f9d891fc7b58790dc02978218b6cb7f59a7d
Gye11 <hilallornekk@gmail.com>
2026-05-03 02:42:03 +0300

    final

---
+++ src/db/models/contacts.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ["work", "home", "personal"],
+      default: "personal",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const Contact = mongoose.model("contact", contactSchema);
+
+export default Contact;
