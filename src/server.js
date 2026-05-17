271167486776a20d912206405b353d657d06b480
Gye11 <hilallornekk@gmail.com>
2026-05-18 02:26:26 +0300

    final

--- src/server.js
+++ src/server.js
@@ -1,24 +1,8 @@
 import dotenv from "dotenv/config";
-import express from "express";
-import contactsRouter from "./routers/contacts.js";
-import notFoundHandler from "./middlewares/notFoundHandler.js";
-import errorHandler from "./middlewares/errorHandler.js";
+import app from "./app.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
 
-const app = express();
 const PORT = Number(process.env.PORT) || 3000;
-
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({
-    message: "Server is running",
-  });
-});
-
-app.use("/contacts", contactsRouter);
-
-app.use(notFoundHandler);
-app.use(errorHandler);
 
 await initMongoConnection();
 
