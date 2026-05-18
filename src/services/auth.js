7dd713598754e162ec21b7db35c0f1848b4ccf77
Gye11 <hilallornekk@gmail.com>
2026-05-18 03:00:15 +0300

    final

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
 import jwt from "jsonwebtoken";
 
-import UserCollection from "../db/models/user.js";
+import { UserCollection } from "../db/models/user.js";
 import { SessionCollection } from "../db/models/session.js";
 import { createSession } from "../utils/createSession.js";
 
