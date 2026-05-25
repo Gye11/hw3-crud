b2c3d0d9968bba8053284476deb7e3235a45f21f
Gye11 <hilallornekk@gmail.com>
2026-05-26 02:40:46 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
 
 import authRouter from "./routers/auth.js";
 import contactsRouter from "./routers/contacts.js";
 
 const app = express();
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync("./docs/swagger.json", "utf-8"),
+);
 
 app.use(
   cors({
@@ -16,6 +22,8 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/auth", authRouter);
 app.use("/contacts", contactsRouter);
